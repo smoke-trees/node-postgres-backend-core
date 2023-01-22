80d35afaa8a65072cdb7b892a566eb745ba93204
Anshuman Chhapolia <anshuman@smoketrees.in>
2023-01-22 14:38:08 +0530

    feat: beautify logs
    Signed-off-by: Anshuman Chhapolia <anshuman@smoketrees.in>

--- src/core/log.ts
+++ src/core/log.ts
@@ -6,50 +6,68 @@ const contextFormat = format((info) => {
   info.traceId = context?.traceId
   return info
 })
+const debugFormat = format.printf(info => {
+  let out = `${info.timestamp}: ${info.level}: ${info.message}`;
+
+  if (info.traceId) {
+    out = out + '\n Trace Id: ' + info.traceId;
+  }
+  if (info.functionName) {
+    out = out + '\n Function Name: ' + info.functionName;
+  }
+  if (info?.error) {
+    out = out + '\n Error Stack: ' + info.error;
+  }
+  if (info.metadata) {
+    out = out + '\n Metadata:' + JSON.stringify(info.metadata)
+  }
+  return out;
+})
 
 const wTransports = [
   new transports.Console({
-    level: 'silly',
     handleExceptions: true,
     format: process.env.NODE_ENV === 'production'
       ? format.combine(contextFormat(), format.timestamp(), format.json())
-      : format.combine(contextFormat(), format.timestamp(), format.colorize(), format.simple())
+      : format.combine(contextFormat(), format.timestamp(), format.colorize(),
+        format.metadata({ fillExcept: ['message', 'timestamp', 'functionName', 'error', 'level', 'traceId'] }),
+        debugFormat,
+      )
   })
 ]
 
 const logger = createLogger({
-  level: 'silly',
   transports: wTransports
 })
 
 class CustomLogger {
   private _logger: Logger
 
-  public get logger (): Logger {
+  public get logger(): Logger {
     return this._logger
   }
 
-  public set logger (value: Logger) {
+  public set logger(value: Logger) {
     this._logger = value
   }
 
-  constructor (logger: Logger) {
+  constructor(logger: Logger) {
     this._logger = logger
   }
 
-  info (message: string, functionName?: string, meta?: any): void {
+  info(message: string, functionName?: string, meta?: any): void {
     this._logger.info(message, { functionName, ...meta })
   }
 
-  debug (message: string, functionName?: string, meta?: any): void {
+  debug(message: string, functionName?: string, meta?: any): void {
     this._logger.debug(message, { functionName, ...meta })
   }
 
-  trace (message: string, functionName?: string, meta?: any): void {
+  trace(message: string, functionName?: string, meta?: any): void {
     this._logger.error(message, { functionName, ...meta })
   }
 
-  error (message: string, functionName?: string, error?: unknown, meta?: any): void {
+  error(message: string, functionName?: string, error?: unknown, meta?: any): void {
     this._logger.error(message, {
       error: error instanceof Error && error.stack,
       functionName,
@@ -57,15 +75,15 @@ class CustomLogger {
     })
   }
 
-  warn (message: string, functionName?: string, meta?: any): void {
+  warn(message: string, functionName?: string, meta?: any): void {
     this._logger.warn(message, { functionName, ...meta })
   }
 
-  fatal (message: string, functionName?: string, meta?: any): void {
+  fatal(message: string, functionName?: string, meta?: any): void {
     this._logger.error(message, { functionName, ...meta })
   }
 }
 
-export const log =  new CustomLogger(logger)
+export const log = new CustomLogger(logger)
 
 export default log
